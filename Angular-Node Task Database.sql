707184d6bba0e1a02fc816581ea1c151754b8697
VikyathKumar2001 <72591517+VikyathKumar2001@users.noreply.github.com>
2023-02-02 11:22:42 +0530

    Add files via upload

---
+++ Angular-Node Task Database.sql
@@ -0,0 +1,15 @@
+create table Empdetails(
+empcode integer,
+name varchar(20),
+designation varchar(20),
+email varchar(20),
+location varchar(20)
+);
+
+insert into Empdetails values (1,'Vikyath','web-developer','vikyath@gmail.com','Pune');
+insert into Empdetails values (2,'Vikram','Manager','vikram@gmail.com','Chennai');
+insert into Empdetails values (3,'Rolex','HR','rolex@gmail.com','US');
+insert into Empdetails values (4,'Dhilli','Driver','Dhilli@gmail.com','Tirchy');
+
+select * from Empdetails;
+
